776c25c615fb3af77a090454da3391785703ef28
omers <omershay1212@gmail.com>
2025-01-29 22:08:44 +0200

    update

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
-export const SPEECH_KEY = "EgkxRJu9uRMdvlG5dGJvVxka3C1j4E56Vt8cadgYODj61hG6qmJoJQQJ99BAACYeBjFXJ3w3AAAYACOGqjCf";
-export const SPEECH_REGION = "eastus";
+export const SPEECH_KEY = "";
+export const SPEECH_REGION = "";
